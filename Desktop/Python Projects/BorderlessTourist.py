415d22f9968c5576d6705c830ce610b96834f9b5
Georgekuz123 <georgek@Georges-MBP.home>
2020-05-17 18:59:18 -0400

    Added interest finder logic and find attractions in city function

--- Desktop/Python Projects/BorderlessTourist.py
+++ Desktop/Python Projects/BorderlessTourist.py
@@ -42,3 +42,24 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 print(attractions)
+
+"""This function takes in a destination and the traveler's interests.
+From there the index of the dest is taken and the possible attractions there
+are returned.The possible interests are compared with user interests and
+the landmark that fits is returned."""
+
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for possible_attraction in attractions_in_city:
+    attraction_tags = possible_attraction[1] 
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+  
+  
+la_arts = find_attractions('Los Angeles, USA', ['art'])
+
+print(la_arts)
